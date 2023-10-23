84f59c371da77796fa8a17920a0b5d175729366f
ribbal <balbir1@gmail.com>
2023-10-23 22:01:52 +0100

    fix typo

--- src/Marten.Testing/Harness/ConnectionSource.cs
+++ src/Marten.Testing/Harness/ConnectionSource.cs
@@ -6,7 +6,7 @@ public class ConnectionSource : ConnectionFactory
 {
     // Keep the default timeout pretty short
     public static readonly string ConnectionString = Environment.GetEnvironmentVariable("marten_testing_database")
-                                                     ?? "Host=localhost;Port=5432;Database=marten_testing;Username=postgres;password=password;Command Timeout=5";
+                                                     ?? "Host=localhost;Port=5432;Database=marten_testing;Username=postgres;password=postgres;Command Timeout=5";
 
     static ConnectionSource()
     {
